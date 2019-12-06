52910071ba9562eb3994eb73189151bdd38301e3
Mark Thomas <MarkTee@users.noreply.github.com>
2019-12-06 13:06:10 -0700

    Use files rather than stdin to get input

--- day1/day1_1.py
+++ day1/day1_1.py
@@ -5,12 +5,10 @@ def module_fuel(mass):
 def main():
     total_fuel = 0
     # Calculate fuel for all modules
-    while True:
-        try:
-            module_mass = int(input())
+    with open('1.in') as file:
+        for line in file:
+            module_mass = int(line)
             total_fuel += module_fuel(module_mass)
-        except EOFError:
-            break
     print(total_fuel)
 
 if __name__ == '__main__':
