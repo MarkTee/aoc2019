13758c4a7a18c7580ac342b0a77aad9d995a5832
Mark Thomas <MarkTee@users.noreply.github.com>
2019-12-06 17:58:58 -0700

    Solve Day 4 - Part 1

---
+++ day4/day4_1.py
@@ -0,0 +1,45 @@
+from collections import Counter
+
+def decreasing(num):
+    """Returns True if any adjacent digits in a number are in increasing order."""
+    assert num >= 0 # only examine nonnegative numbers
+
+    while num >= 10:
+        last_digit = num % 10
+        num //= 10
+        if last_digit < num % 10:
+            return True
+    return False
+
+def is_valid(num):
+    """Returns True if a number satisfies the password criteria."""
+    num_str = str(num)
+    c = Counter(num_str)
+    # numbers with decreasing digits are invalid
+    if decreasing(num):
+        return 0
+    '''
+    valid numbers must have at least two of the same digit; since at the point
+    we know the number's digits are non-decreasing, then any repeated
+    characters must also be adjacent to each other
+    '''
+    return max(c.values()) >= 2
+
+def main():
+    lower = 367479
+    upper = 893698
+
+    valid_numbers = list(filter(is_valid, range(lower, upper + 1)))
+    print(len(valid_numbers))
+
+def test():
+    assert decreasing(223450) == True
+    assert decreasing(111111) == False
+    assert decreasing(123789) == False
+    assert decreasing(10) == True
+    assert decreasing(0) == False
+    assert decreasing(5) == False
+
+if __name__ == '__main__':
+    # test()
+    main()
