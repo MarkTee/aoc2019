64f77bb9608a8789d812ef92a5a109f6d2b83245
Mark Thomas <MarkTee@users.noreply.github.com>
2019-12-08 13:01:53 -0700

    Place program in separate function and add test

--- _day8/day8_2.py
+++ _day8/day8_2.py
@@ -1,12 +1,8 @@
 from itertools import chain
 
-def main():
-    # get image
-    with open('8.in') as f:
-        image = f.readline().rstrip()
-
+def print_decoded_image(image, col_length, row_length):
     # separate image into layers
-    layer_size = 25 * 6
+    layer_size = col_length * row_length
     layers = [image[i:i + layer_size] for i in range(0, len(image), layer_size)]
     # flatten layers
     flattened_image = [''.join(pixel) for pixel in zip(*layers)]
@@ -16,15 +12,27 @@ def main():
     decoded_image = [pixel[0] for pixel in opaque_image]
 
     # print the decoded image to the terminal
-    for row in range(6):
-        for col in range(25):
+    for row in range(row_length):
+        for col in range(col_length):
             # print black pixel
-            if decoded_image[(row * 25) + col] == '0':
+            if decoded_image[(row * col_length) + col] == '0':
                 print(u"\u2588", end='')
             # print white pixel
             else:
                 print(u"\u2591", end='')
         print()
 
+def main():
+    # get image
+    with open('8.in') as f:
+        image = f.readline().rstrip()
+
+    print_decoded_image(image, 25, 6)
+
+def test():
+    image = '0222112222120000'
+    print_decoded_image(image, 2, 2)
+
 if __name__ == '__main__':
+    # test()
     main()
