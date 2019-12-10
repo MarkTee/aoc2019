5e4abef06c27b11d9a28a16e43927f671656435b
Mark Thomas <MarkTee@users.noreply.github.com>
2019-12-09 18:48:36 -0700

    Solve Day 9 - Part 1

--- _day9/IntcodeComputer.py
+++ _day9/IntcodeComputer.py
@@ -4,7 +4,7 @@ class IntcodeComputer():
     """An implementation of the Intcode Computer described in Advent of Code 2019."""
 
     def __init__(self, program):
-        self.program = program + [0] * 500
+        self.program = program + [0] * 2000
         self.current_opcode = None
         self.pc = 0 # program counter
         self.jump_table = {1: self.op_1,
@@ -34,7 +34,10 @@ class IntcodeComputer():
         return self.program
 
     def get_parameters(self, n):
-        """Returns the next n parameters following the current opcode."""
+        """Returns the next n parameters following the current opcode.
+        Used to obtain "get" parameters (i.e. parameters that an instruction reads from)
+        """
+
         # pad opcode with zeros to determine parameter modes easier
         opcode = str(self.current_opcode).zfill(2 + n)
 
@@ -43,23 +46,46 @@ class IntcodeComputer():
         for i in range(1, n + 1):
             mode = opcode[-2 - i]
 
+            parameter_register_value = self.program[self.pc + i]
+
             # position mode
             if mode == '0':
-                parameter_register = self.program[self.pc + i]
-                parameter = self.program[parameter_register]
+                parameter = self.program[parameter_register_value]
 
             # immediate mode
             elif mode == '1':
-                parameter = self.program[self.pc + i]
+                parameter = parameter_register_value
 
-            # position mode
+            # relative mode
             elif mode == '2':
-                offset = self.program[self.pc + i]
-                parameter = self.program[self.relative_base + offset]
+                parameter = self.program[self.relative_base + parameter_register_value]
 
             parameters.append(parameter)
 
         return parameters
+
+    def set_parameter(self, n):
+        """Returns the nth parameter for the current opcode. n = 1 or n = 3
+        Used to obtain "set" parameters (i.e. parameters that an instruction writes to)
+
+        The following post was very helpful in implementing this method:
+        https://old.reddit.com/r/adventofcode/comments/e8aw9j/2019_day_9_part_1_how_to_fix_203_error/faajho3/
+        """
+        # pad opcode with zeros to determine parameter modes easier
+        opcode = str(self.current_opcode).zfill(2 + n)
+        # determine parameter mode
+        mode = opcode[-2 - n]
+
+
+        parameter_register_value = self.program[self.pc + n]
+
+        # position mode
+        if mode == '0':
+            return parameter_register_value
+        # relative mode
+        elif mode == '2':
+            return self.relative_base + parameter_register_value
+
 
     #############################
     ########## Opcodes ##########
@@ -68,20 +94,20 @@ class IntcodeComputer():
     def op_1(self):
         """Addition Operation"""
         addend1, addend2 = self.get_parameters(2)
-        output_register = self.program[self.pc + 3]
+        output_register = self.set_parameter(3)
         self.program[output_register] = addend1 + addend2
         self.pc += 4
 
     def op_2(self):
         """Multiplication Operation"""
         multiplicand1, multiplicand2  = self.get_parameters(2)
-        output_register = self.program[self.pc + 3]
+        output_register = self.set_parameter(3)
         self.program[output_register] = multiplicand1 * multiplicand2
         self.pc += 4
 
     def op_3(self):
         """Input Operation"""
-        save_to_register = self.program[self.pc + 1]
+        save_to_register = self.set_parameter(1)
         self.program[save_to_register] = int(input())
         self.pc += 2
 
@@ -110,7 +136,7 @@ class IntcodeComputer():
     def op_7(self):
         """Less Than Operation"""
         parameter1, parameter2 = self.get_parameters(2)
-        output_register = self.program[self.pc + 3]
+        output_register = self.set_parameter(3)
 
         if parameter1 < parameter2:
             self.program[output_register] = 1
@@ -122,7 +148,7 @@ class IntcodeComputer():
     def op_8(self):
         """Equals Operation"""
         parameter1, parameter2 = self.get_parameters(2)
-        output_register = self.program[self.pc + 3]
+        output_register = self.set_parameter(3)
 
         if parameter1 == parameter2:
             self.program[output_register] = 1
