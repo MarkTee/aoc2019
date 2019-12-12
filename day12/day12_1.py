9fe5a9e1ba723b25c1443924fe3b21b024eee08b
Mark Thomas <MarkTee@users.noreply.github.com>
2019-12-11 22:20:51 -0700

    Day 12 - Parse input file

---
+++ day12/day12_1.py
@@ -0,0 +1,32 @@
+def scan_moons(filename):
+    """Parses an input file and extracts the coordinates of each moon.
+    Returns two dicts, which map the i-th moon to its (x, y, z) coordinates,
+    and its initial velocity (which is always 0), respectively.
+    """
+    moon_positions = dict()
+    moon_velocities = dict()
+    # iterate over the input file, which contains one moon per line
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            moon = line.rstrip().split()
+
+            # extract the (x, y, z) coordinates for the current moon
+            x = int(moon[0][moon[0].index('=') + 1:-1])
+            y = int(moon[1][moon[1].index('=') + 1:-1])
+            z = int(moon[2][moon[2].index('=') + 1:-1])
+
+            moon_positions[i] = (x, y, z)
+            moon_velocities[i] = 0
+    return moon_positions, moon_velocities
+
+def main():
+    filename = '12.in'
+    moon_positions, moon_velocities = scan_moons(filename)
+
+def test():
+    filename = 'test1.in'
+    moon_positions, moon_velocities = scan_moons(filename)
+
+if __name__ == '__main__':
+    test()
+    # main()
