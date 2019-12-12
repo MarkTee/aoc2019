28b796de0dac3d9241bbe31d3e931c2095e129bb
Mark Thomas <MarkTee@users.noreply.github.com>
2019-12-12 14:20:22 -0700

    Implement gravity, velocity, and debug functions

--- day12/day12_1.py
+++ day12/day12_1.py
@@ -1,10 +1,10 @@
 def scan_moons(filename):
     """Parses an input file and extracts the coordinates of each moon.
-    Returns two dicts, which map the i-th moon to its (x, y, z) coordinates,
-    and its initial velocity (which is always 0 on all 3 axes), respectively.
+
+    Returns a dict mapping the i-th moon to a list containing its (x, y, z)
+    coordinates and its velocity (which is initially 0 on all 3 axes).
     """
-    moon_positions = dict()
-    moon_velocities = dict()
+    moons = dict()
     # iterate over the input file, which contains one moon per line
     with open(filename) as f:
         for i, line in enumerate(f):
@@ -15,25 +15,69 @@ def scan_moons(filename):
             y = int(moon[1][moon[1].index('=') + 1:-1])
             z = int(moon[2][moon[2].index('=') + 1:-1])
 
-            moon_positions[i] = (x, y, z)
-            moon_velocities[i] = (0, 0, 0)
-    return moon_positions, moon_velocities
+            moons[i] = [[x, y, z], [0,0,0]]
+    return moons
 
-def apply_gravity(moon_positions, moon_velocities):
+def apply_gravity(moons):
     """Applies gravity (as described in the problem statement)."""
-    pass
+    n_moons = len(moons) # number of moons
 
-def apply_velocity(moon_positions, moon_velocities):
+    # iterate over each moon
+    for i in range(n_moons - 1):
+        moon1_pos = moons[i][0]
+        # iterate over each of the other moons
+        for j in range(i + 1, n_moons):
+            moon2_pos = moons[j][0]
+
+            # iterate over (x, y, z) coordinate and update velocity as necessary
+            for coord in range(3):
+                if moon1_pos[coord] > moon2_pos[coord]:
+                    moons[i][1][coord] -= 1
+                    moons[j][1][coord] += 1
+                elif moon1_pos[coord] < moon2_pos[coord]:
+                    moons[i][1][coord] += 1
+                    moons[j][1][coord] -= 1
+
+def apply_velocity(moons):
     """Adds the velocity of each moon to its own position."""
-    pass
+    for moon in moons.values():
+        for coord in range(3):
+            moon[0][coord] += moon[1][coord]
+
+def step(moons, n, debug=False):
+    """Advances the simulation n steps."""
+    if debug:
+        print_debug(moons, 0)
+
+    for i in range(n):
+        apply_gravity(moons)
+        apply_velocity(moons)
+
+        if debug:
+            print_debug(moons, i + 1)
+
+def print_debug(moons, i):
+    """Prints the current state of the simulation."""
+    print("After {} steps:".format(i))
+    for moon in moons:
+        moon = moons[moon]
+        print("pos=<x={:3d}, y={:3d}, z={:3d}>, ".format(moon[0][0],
+                                                         moon[0][1],
+                                                         moon[0][2]), end='')
+        print("vel=<x={:3d}, y={:3d}, z={:3d}>".format(moon[1][0],
+                                                       moon[1][1],
+                                                       moon[1][2]))
+    print()
 
 def main():
     filename = '12.in'
-    moon_positions, moon_velocities = scan_moons(filename)
+    moons = scan_moons(filename)
+    step(moons, 1000)
 
 def test():
     filename = 'test1.in'
-    moon_positions, moon_velocities = scan_moons(filename)
+    moons = scan_moons(filename)
+    step(moons, 10, debug=True)
 
 if __name__ == '__main__':
     test()
